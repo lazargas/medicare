9ed28f1553f2d0f6f6bdb654b2bff1031747da0b
Akarsh <akarsh.srivastava@inspirebrands.com>
2024-12-02 01:18:34 +0530

    Bugs and ads left

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -6,13 +6,11 @@ import { getBlogs, getTags } from "@/lib/api";
 
 export default async function Layout({children}:Readonly<{children:React.ReactNode}>) {
 
-    const tags = await getTags();
-    const blogs = await getBlogs();
+   
 return (
     <>
     <BlogProvider>
     <Navbar/>    
-    <SearchNav tags={tags} blogs={blogs} />
     <main>{children}</main>
     </BlogProvider>
     </>
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,6 +1,6 @@
 import HomePage from "@/components/atoms/Home";
 import { auth } from "@/auth";
-import { getTags, getUserByEmail } from "@/lib/api";
+import { getBlogs, getTags, getUserByEmail } from "@/lib/api";
 import SearchNav from "@/components/atoms/SearchBar";
 
 interface Tag {
@@ -11,10 +11,13 @@ interface Tag {
 export default async function Home() {
   const session = await auth();
   const userDAta = await getUserByEmail(session?.user?.email!);
+  const tags = await getTags();
+  const blogs = await getBlogs();
   console.log(userDAta); 
   //md:w-[75%]
   return (
     <>
+     <SearchNav tags={tags} blogs={blogs} />
       <div className="flex flex-col md:flex-row w-full h-auto">
         <div className="w-full ">
      
