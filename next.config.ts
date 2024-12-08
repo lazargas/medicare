fd0bce7adccc027a8ea752b7666d8e2278683805
Akarsh <akarsh.srivastava@inspirebrands.com>
2024-12-08 18:21:30 +0530

    Added Cors Check

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,20 @@ const nextConfig: NextConfig = {
     images: {
         domains: ['lh3.googleusercontent.com','medicaldialougearticleimages.s3.amazonaws.com'],
       },
-      
+      async headers (){
+        return [
+          {
+            source:"/api/:path*",
+            headers:[
+              {key:"Access-Control-Allow-Credentials",value:"true"},
+              {key:"Access-Control-Allow-Origin",value:'*'},
+              {key:"Access-Control-Allow-Methods",value:'GET,DELETE,PATCH,POST,PUT'},
+              {key:"Access-Control-Allow-Origin",value:'*'},
+              {key:"Access-Control-Allow-Headers",value:'*'}
+            ]
+          }
+        ]
+      }
 };
 
 export default nextConfig;
